5958c9a7f0633de84bde43680a3ab21279fcafbb
Robert Jensen <jensen.robert@gmail.com>
2012-09-19 15:41:51 +0200

    Plot of the first spectrum of dibenzotheophene

---
+++ defaults.py
@@ -0,0 +1,14 @@
+y_axis_font = 9
+x_axis_font = 9
+labelsize = 8
+
+ratio = 0.618#Golden mean
+width = 12
+pad = 2
+ticklength = 4
+
+left_room = 0.15
+bottom_room = 0.15
+right_room = 0.9
+
+arrowfont = 10
---
+++ std_header.py
@@ -0,0 +1,27 @@
+import sys
+try:
+    import defaults as d
+except:
+    sys.path.append('../')
+    import defaults as d
+    
+import matplotlib
+if 'backend' in locals():
+    matplotlib.use(backend)
+import matplotlib.pyplot as plt
+import numpy as np
+import MySQLdb
+from scipy import optimize
+from scipy import interpolate
+import math
+import matplotlib.gridspec as gridspec
+from matplotlib.patches import Rectangle
+
+matplotlib.rc('text',usetex=True) # Magic fix for the font warnings
+
+try:
+    db = MySQLdb.connect(host="servcinf", user="cinf_reader",passwd = "cinf_reader", db = "cinfdata")
+except:
+    db = MySQLdb.connect(host="127.0.0.1", port=9995, user="cinf_reader",passwd = "cinf_reader", db = "cinfdata")
+
+cursor = db.cursor()

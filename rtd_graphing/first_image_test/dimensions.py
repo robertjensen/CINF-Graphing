cc12f27bf0769d47ba9065ad3b10c3a7e86fa891
Robert Jensen <jensen.robert@gmail.com>
2013-01-14 22:15:13 +0100

    New parameter: Rotation

--- rtd_graphing/first_image_test/dimensions.py
+++ rtd_graphing/first_image_test/dimensions.py
@@ -4,8 +4,13 @@ crop['right']  = 115
 crop['top']    = 110
 crop['bottom'] = 90
 
+rotation = -8
 left_edge = 10
 right_edge = 132
 
 images = {}
+images[21.9796030521] = [24,31]
+images[138.837656021] = [24,42]
+images[177.585380077] = [24,52]
 images[216.845384121] = [24,64]
+images[258.022848129] = [25,80]
